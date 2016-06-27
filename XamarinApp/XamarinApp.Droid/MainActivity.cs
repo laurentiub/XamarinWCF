8f8296fc928c8224e83a8c87e57e156753eeffe9
admin <laurentiu@eigodev.ro>
2016-06-28 00:05:23 +0300

    Add project files.

---
+++ XamarinApp/XamarinApp.Droid/MainActivity.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using XamarinApp.Droid;
+
+[assembly:Xamarin.Forms.Dependency(typeof(AccessService))]
+namespace XamarinApp.Droid
+{
+    [Activity(Label = "XamarinApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+
+    public class AccessService : IAccessService
+    {
+        public string GetData(int i)
+        {
+            var proxy = ChannelFactory<IService1>.CreateChannel(new BasicHttpBinding(),
+    new EndpointAddress("http://test.csharpdeveloper.ninja/Service1.svc"));
+
+            return proxy.GetData(4);
+        }
+    }
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
+
---
+++ XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            var service = DependencyService.Get<IAccessService>();
+            var response = service.GetData(5);
+
+
+            MyLabel.Text = response;
+        }
+    }
+}
